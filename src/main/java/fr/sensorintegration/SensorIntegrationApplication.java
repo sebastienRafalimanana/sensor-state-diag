1d79f3c54f0f7ca8daa0d11b189e3b87265ad5fc
Rafalimanana Sébastien <sebastienrafalimanana@gmail.com>
2025-06-13 18:57:15 +0300

    chore:init project

---
+++ src/main/java/fr/sensorintegration/SensorIntegrationApplication.java
@@ -0,0 +1,13 @@
+package fr.sensorintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/sensorintegration/SensorIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sensorintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
